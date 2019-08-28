cb0b47e534b3b25b11dfd88390a685307ea8aeae
2ndyrGroupC <2ndyrGroupC@pn.local>
2019-08-28 16:31:38 +0800

    kani

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,17 +1,40 @@
 // basic functionalities
-client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt")
-client.subscribe("mqtt/demo")
+$(document).ready(function(){
+  $(".connect").click(function () {
+    client = mqtt.connect($("#address").val())
+    client.subscribe("mqtt/demo")
+    client.on("connect", function () {
+      console.log("Successfully connected");
+    })
+    client.on("message", function (topic, payload) {
+      // var add= "<tr><td>" +topic+ "</td><td>" +payload+ "</td></tr>";
+      // $("table tbody").append(add);
+      console.log([topic, payload].join(": "));  
+    })
+    client.publish("mqtt/demo", "hello world!")
+  
+    
+    
+  })
 
-client.on("connect", function(){
-    console.log("Successfully connected");
+  $(".publish").click(function(){
+    var ttopic=$("#ptopic").val();
+    var payload=$("#pload").val();
+    client.publish("mqtt/"+ ttopic, payload);
+    // var ptopic=$("#ptopic").val();
+    // var payload = $("#pload").val();
+    // // var time=
+    // var add= "<tr><td>" +ptopic+ "</td><td>" +payload+ "</td></tr>";
+    // $("table tbody").append(add);
+  })
+  $(".publish").click(function(){
+
+  })
 })
 
-client.on("message", function (topic, payload) {
-  console.log([topic, payload].join(": "));
-  client.end();
-})
 
-client.publish("mqtt/demo", "hello world!")
+
+
 
 // // advance functionalities
 // client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt")
