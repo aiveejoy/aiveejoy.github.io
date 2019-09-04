a7ec533a1f2b201f53fc8c8a4160fc85c97c1f51
2ndyrGroupC <2ndyrGroupC@pn.local>
2019-09-04 15:11:15 +0800

    modiifed

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,35 +1,42 @@
-// basic functionalities
 $(document).ready(function(){
   $(".connect").click(function () {
-    client = mqtt.connect($("#address").val())
-    client.subscribe("mqtt/demo")
+    client = mqtt.connect($("#address").val());
     client.on("connect", function () {
       console.log("Successfully connected");
+      $("#status").val("Successfully connected");
     })
     client.on("message", function (topic, payload) {
-      // var add= "<tr><td>" +topic+ "</td><td>" +payload+ "</td></tr>";
-      // $("table tbody").append(add);
-      console.log([topic, payload].join(": "));  
-    })
-    client.publish("mqtt/demo", "hello world!")
+      var dt = new Date();
+      var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds(); 
+       console.log("recieved:\ntopic: "+topic+"\npayload: "+payload);   
+      $("#tbody1").append("<tr><td>"+topic+"</td><td>"+payload+"</td><td>"+time+"</td></tr>");       
+    }) 
+  })
+
+  $(".disconnect").click(function(){
+      client.end();
+      $("#status").val("Client Disconnected!");
+  })
+
+  $(".publish").click(function(){
+    var dt = new Date();
+    var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
+    client.publish($("#ptopic").val(),$("#pload").val());
+    // console.log("published:\ntopic: "+$("#ptopic").val()+"\npayload: "+$("#pload").val());
+    $("#tbody2").append("<tr><td>"+$("#ptopic").val()+"</td><td>"+$("#pload").val()+"</td><td>"+time+"</td></tr>");
+    
+  })
+  $(".subscribe ").click(function(){
+    var dt = new Date();
+    var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
+    client.subscribe($("#stopic").val());
+    $("#tbody3").append("<tr><td>"+$("#stopic").val()+"</td><td>"+time+"</td></tr>");
+    
+  });
   
-    
-    
-  })
+  $(".unsubscribe").click(function(){
+    client.unsubscribe($("#stopic").val());
 
-  $(".publish").click(function(){
-    var ttopic=$("#ptopic").val();
-    var payload=$("#pload").val();
-    client.publish("mqtt/"+ ttopic, payload);
-    // var ptopic=$("#ptopic").val();
-    // var payload = $("#pload").val();
-    // // var time=
-    // var add= "<tr><td>" +ptopic+ "</td><td>" +payload+ "</td></tr>";
-    // $("table tbody").append(add);
-  })
-  $(".publish").click(function(){
-
-  })
 })
 
 
@@ -62,3 +69,4 @@ $(document).ready(function(){
 //     console.log("published")
 //   }
 // })
+})
